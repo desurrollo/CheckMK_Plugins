5c03062aa44874c8c129b612b93ee60777dd5ae3
Desurrollo <3084047+desurrollo@users.noreply.github.com>
2024-06-05 11:05:39 +0200

    Update agent_unity.py
    Parser configuration

--- Special_Agents/DELL_Unity/agents/special/agent_unity.py
+++ Special_Agents/DELL_Unity/agents/special/agent_unity.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python3
 
+import argparse
 import requests
 
+### MAIN FUNCTION ###
+# ARGUMENTS:
+# ipaddr --> IP address of the Unity appliance management interface
+# user --> API user name
+# password --> API user password
 if __name__ == '__main__':
+     parser = argparse.ArgumentParser()
+     parser.add_argument("-a", "--ipaddr", type=ip_address, required=True, help="Host IP address")
+     parser.add_argument("-u", "--user", type=str, required=True, help="API user name")
+     parser.add_argument("-p", "--password", type=str, required=True, help="API user password")
+     args = parser.parse_args()
+     
      print("<<<unity_api_status")
